3c16adfcb37ea9029946b546c34ce03cdc5afb34
jm underwood <jm.underwood@hotmail.com>
2025-07-18 15:46:15 -0400

    Add project files.

---
+++ Rectangle Properties Program.cpp
@@ -0,0 +1,26 @@
+// Rectangle Properties Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int getLength_Width();
+
+}
+
+int getLength_Width(int length, int width)
+{
+    cout << "Enter the length \n";
+    cin >> length;
+    cout << "Enter the width \n";
+    cin >> width;
+}
+
+/*int calcPerimeter(int length, int width)
+{
+    getLength_Width(length, width)
+    int Perimeter = length * width;
+    cout << "The perimeter is " << Perimeter << endl;
+}*/
