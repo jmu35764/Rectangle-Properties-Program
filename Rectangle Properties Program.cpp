a8757e0761042a7824b0e57abcf44432b23a6e37
jm underwood <jm.underwood@hotmail.com>
2025-07-18 17:07:34 -0400

    Code Iteration

--- Rectangle Properties Program.cpp
+++ Rectangle Properties Program.cpp
@@ -2,25 +2,59 @@
 //
 
 #include <iostream>
+
+
 using namespace std;
 
-int main()
-{
-   int getLength_Width();
-
-}
-
-int getLength_Width(int length, int width)
+void getLength_Width(int& length, int& width)
 {
     cout << "Enter the length \n";
     cin >> length;
+    while (length <= 0)
+    {
+       cout << "The value must be positive" << endl;
+       cin >> length;
+    }
+  
     cout << "Enter the width \n";
     cin >> width;
+
+    while (width <= 0)
+    {
+        cout << "The value must be positive" << endl;
+        cin >> width;
+    }
+  
 }
 
-/*int calcPerimeter(int length, int width)
+int calcPerimeter(int length, int width)
 {
-    getLength_Width(length, width)
-    int Perimeter = length * width;
-    cout << "The perimeter is " << Perimeter << endl;
-}*/
+    //getLength_Width(length, width);
+    return 2*(length + width);
+}
+
+int calcArea(int length, int width)
+{
+    return length * width;
+}
+
+void displayProperties(int perimeter, int area)
+{
+    cout << "The perimeter is " << perimeter << endl;
+    cout << "The area is " << area << endl;
+}
+
+int main()
+{
+    int reclength, recwidth;
+
+    getLength_Width(reclength, recwidth);
+    int Rec_perimeter = calcPerimeter(reclength, recwidth);
+    int Rec_area = calcArea(reclength, recwidth);
+    
+    displayProperties(Rec_perimeter, Rec_area);
+}
+
+
+
+
